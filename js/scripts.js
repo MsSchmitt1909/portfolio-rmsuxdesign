65a368947d185dc9dd552d97ddb9efb9604363c2
Anil Kumar <anilbms75@gmail.com>
2021-05-03 20:42:28 +0200

    Add carousel functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,55 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+
+
+  // Carousel implementation
+  $(document).ready(() => {
+    // var carouselsId = ['carouselExampleIndicators-1', 'carouselExampleIndicators-2', 'carouselExampleIndicators-3']
+
+    // carouselsId.forEach(carouselId => {
+    //     var myCarousel = document.querySelector(`#${carouselId}`)
+    //     var carousel = new bootstrap.Carousel(myCarousel,  {
+    //       interval: 3000
+    //     })
+    //     // prev
+    //     $('.carousel-control-prev').click((event) => {
+    //       carousel.prev();
+    //     })
+        
+    //       // next
+    //     $('.carousel-control-next').click((event) => {
+    //       carousel.next();
+    //     })
+    //   })
+
+      //firstCarousel
+      var myCarouselFirst = document.querySelector('#carouselExampleIndicators-1')
+      var carouselFirst = new bootstrap.Carousel(myCarouselFirst,  {
+        interval: 3000
+      })
+      // prev
+      $('.carousel-control-prev').click((event) => {
+        carouselFirst.prev();
+      })
+      
+        // next
+      $('.carousel-control-next').click((event) => {
+        carouselFirst.next();
+      })
+
+      //secondCarousel
+      var myCarouselSecond = document.querySelector('#carouselExampleIndicators-2')
+      var carouselSecond = new bootstrap.Carousel(myCarouselSecond,  {
+        interval: 3000
+      })
+      // prev
+      $('.carousel-control-prev').click((event) => {
+        carouselSecond.prev();
+      })
+      
+        // next
+      $('.carousel-control-next').click((event) => {
+        carouselSecond.next();
+      })
+  });
